d7d76a8f6dceab768c15616569ebecd5785ba36c
Sean Warlick <warlick.sean@gmail.com>
2024-10-30 19:38:50 -0700

    Created a subtract method.

--- file2.py
+++ file2.py
@@ -1,14 +1,16 @@
-another_function():
+def another_function():
     pass
 
 class MyClass():
 
     def __init__(self, x, y):
-	self.x = x
-	self.y = y
+        self.x = x
+        self.y = y
+    def multipy(self, ):
+	    return x * y
+    
+    def add(self):
+	    return x + y
 
-    def multipy():
-	return x * y
-
-    def add():
-	return x + y
+    def subtract(self):
+		return x - y
